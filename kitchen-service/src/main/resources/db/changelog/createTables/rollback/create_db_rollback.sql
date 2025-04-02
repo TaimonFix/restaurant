f78d0fe2f1c271d91a1a8cab2d1c4e94ae5c513b
bratyshevTD <bratyshev2013@mail.ru>
2025-04-02 15:18:26 +0300

    Реализовал добавление миграции при запуске приложения

--- kitchen-service/src/main/resources/db/changelog/createTables/rollback/create_db_rollback.sql
+++ kitchen-service/src/main/resources/db/changelog/createTables/rollback/create_db_rollback.sql
@@ -1,3 +1,3 @@
-drop table if exists kitchen_order;
-drop table if exists dish;
-drop table if exists order_to_dish;
+drop table if exists kitchen_order CASCADE;
+drop table if exists dish CASCADE;
+drop table if exists order_to_dish CASCADE;
